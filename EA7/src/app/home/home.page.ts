60349726dab6ea53c71f8c81a3e1a452d9788627
Mel⭐ <22073@portalcedi.edu.mx>
2025-01-21 10:14:23 -0600

    1st Commit

---
+++ EA7/src/app/home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+  items: { id: number; name: string; date: string, completed: boolean }[] = [];
+  nextId: number = 1;
+
+  constructor(public navCtrl: NavController) { }
+
+}
